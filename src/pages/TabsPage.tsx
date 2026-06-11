46ab920630964a0b9bcf96c8f23b4c37189545a2
anastasiiadns <nastyadenisenko17@gmail.com>
2026-06-11 15:31:15 +0300

    fixed current TabsPage.tsx

--- src/pages/TabsPage.tsx
+++ src/pages/TabsPage.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import { Tab } from '../types/Tab';
-import { TabList } from '../components/Tabs/TabList';
-import { TabPanel } from '../components/Tabs/TabPanel';
+import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
+import classNames from 'classnames';
+import { Link, useParams } from 'react-router-dom';
 
-export const tabs: Tab[] = [
+const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
@@ -18,8 +17,23 @@ export const TabsPage: React.FC = () => {
     <div className="container">
       <h1 className="title">Tabs page</h1>
 
-      <TabList tabs={tabs} selectedTabId={tabId} />
-      <TabPanel selectedTab={selectedTab} />
+      <Tabs className="tabs is-boxed">
+        <TabList>
+          {tabs.map(tab => (
+            <Tab
+              key={tab.id}
+              data-cy="Tab"
+              className={classNames({ 'is-active': tab.id === tabId })}
+            >
+              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+            </Tab>
+          ))}
+        </TabList>
+      </Tabs>
+
+      <TabPanel className="block" data-cy="TabContent">
+        {selectedTab ? selectedTab.content : 'Please select a tab'}
+      </TabPanel>
     </div>
   );
 };
